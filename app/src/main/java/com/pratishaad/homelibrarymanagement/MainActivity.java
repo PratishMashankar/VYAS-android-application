5c5ba1b919c8fb4050a6c848160319a27510757c
Sreevarsh Vasista <vasistasreevarsh1@gmail.com>
2021-05-22 14:36:18 +0530

    clean code till recommendation

--- app/src/main/java/com/pratishaad/homelibrarymanagement/MainActivity.java
+++ app/src/main/java/com/pratishaad/homelibrarymanagement/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -93,9 +92,5 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-    }
-
-    public FirebaseUser getFirebaseUser() {
-        return FirebaseAuth.getInstance().getCurrentUser();
     }
 }
