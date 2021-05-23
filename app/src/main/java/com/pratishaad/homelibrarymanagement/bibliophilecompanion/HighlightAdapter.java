73f1d26f67c47b2de02d13ba3e703f55d2bca1fe
Sreevarsh Vasista <vasistasreevarsh1@gmail.com>
2021-05-23 16:55:03 +0530

    completed View Highlights

---
+++ app/src/main/java/com/pratishaad/homelibrarymanagement/bibliophilecompanion/HighlightAdapter.java
@@ -0,0 +1,88 @@
+package com.pratishaad.homelibrarymanagement.bibliophilecompanion;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.pratishaad.homelibrarymanagement.Book;
+import com.pratishaad.homelibrarymanagement.R;
+
+import java.util.List;
+
+public class HighlightAdapter extends RecyclerView.Adapter<HighlightAdapter.ViewHolder> {
+
+    List<Highlights>articleLists;
+    Context ct;
+    OnRecyclerViewItemClickListener mOnRecyclerViewItemClickListener;
+
+    public HighlightAdapter(){}
+
+    public HighlightAdapter(List<Highlights> articleLists, Context ct, OnRecyclerViewItemClickListener mOnRecyclerViewItemClickListener) {
+        this.articleLists = articleLists;
+        this.ct = ct;
+        this.mOnRecyclerViewItemClickListener=mOnRecyclerViewItemClickListener;
+    }
+
+    public HighlightAdapter(List<Highlights> articleLists, Context applicationContext) {
+        this.articleLists = articleLists;
+        this.ct = ct;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_highlight_list,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Highlights articleList=articleLists.get(position);
+
+        holder.highlightURL.setText("From webpage: "+articleList.getUrl());
+        holder.highlightText.setText(articleList.getHighlight());
+    }
+
+    @Override
+    public int getItemCount() {
+        return articleLists.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView highlightURL;
+        TextView highlightText;
+
+        OnRecyclerViewItemClickListener onRecyclerViewItemClickListener;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            highlightURL=itemView.findViewById(R.id.link_details);
+            highlightText=itemView.findViewById(R.id.highlight_details);
+//            this.onRecyclerViewItemClickListener=onRecyclerViewItemClickListener;
+
+            itemView.setOnClickListener(this);
+
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            onRecyclerViewItemClickListener.onRecyclerViewItemClicked(getAdapterPosition());
+        }
+    }
+
+    public interface OnRecyclerViewItemClickListener
+    {
+        public void onRecyclerViewItemClicked(int position);
+    }
+}
--- app/src/main/java/com/pratishaad/homelibrarymanagement/bibliophilecompanion/ViewHighlights.java
+++ app/src/main/java/com/pratishaad/homelibrarymanagement/bibliophilecompanion/ViewHighlights.java
@@ -1,16 +1,75 @@
 package com.pratishaad.homelibrarymanagement.bibliophilecompanion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pratishaad.homelibrarymanagement.Book;
 import com.pratishaad.homelibrarymanagement.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewHighlights extends AppCompatActivity {
+    FirebaseAuth fAuth;
+    RecyclerView rv;
+    List<Highlights> articleLists;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_highlights);
+
+        fAuth=FirebaseAuth.getInstance();
+
+        fAuth= FirebaseAuth.getInstance();
+        rv = findViewById(R.id.highlights);
+        rv.setHasFixedSize(true);
+
+        Bundle extras=getIntent().getExtras();
+        final String projectname= (String) extras.get("Project Name");
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        DividerItemDecoration decoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rv.addItemDecoration(decoration);
+        articleLists = new ArrayList<>(); //list of highlights i.r article list is of the type List<Highlights>
+        databaseReference= FirebaseDatabase.getInstance().getReference().child(fAuth.getUid()).child("Projects").child(projectname);;
+        getDetails();
+    }
+
+    public void getDetails()
+    {
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                for (DataSnapshot di : snapshot.getChildren()) {
+                    Highlights articleList = di.getValue(Highlights.class);
+                    articleLists.add(articleList);
+                }
+                final HighlightAdapter adapter=new HighlightAdapter(articleLists, getApplicationContext());
+                rv.setAdapter(adapter);
+            }
+            
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
